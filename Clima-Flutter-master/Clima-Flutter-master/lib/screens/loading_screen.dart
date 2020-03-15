fe32d47166456385bb9f90b483ead3f718f42ad7
patrick <patrickchow0803@gmail.com>
2020-03-15 17:13:39 -0400

    Added HTTP package and now makes a get request to a sample website for weather location.

--- Clima-Flutter-master/Clima-Flutter-master/lib/screens/loading_screen.dart
+++ Clima-Flutter-master/Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
 
+// Allows me to access the package by using the variable http.
+// The benefit of this is that it's more clear what package I'm using.
+import 'package:http/http.dart' as http;
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -23,8 +27,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  // Makes an API call to OpenWeatherMap to get weather information.
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
